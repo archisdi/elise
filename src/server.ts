e462663cdef268a8d54b99c72c6cadb7e6c2f55c
Archie Isdiningrat <archie.isdiningrat@bizzy.co.id>
2018-08-08 14:55:49 +0700

    fix server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,57 @@
+/**
+ * Load Environment
+ */
 import env from 'dotenv';
 env.config();
 
-import app from './app';
+/**
+ * Debugger
+ */
+import debug from 'debug';
+debug('express-ts-api-server');
 
+import app from './app';
+import http from 'http';
+
+/**
+ * Get port from environment and store in Express.
+ */
 const port: number | string = process.env.APP_PORT || 3000;
-const server = app.listen(port, () => {
-  console.log(`app is running at http://127.0.0.1:${port}`);
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port);
+server.on('error', (error: any) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 });
 
-export default server;
+const addr = server.address();
+const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+console.log(`server running at ${bind}`);
