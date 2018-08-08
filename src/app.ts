f012d66d4bdd280f12fa167b48ee9e0ba52ae51b
Archie Isdiningrat <archie.isdiningrat@bizzy.co.id>
2018-08-08 15:03:15 +0700

    add plugins

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
 import routesHandler from './routes';
 import exceptionHandler from './exceptions';
 
 const app: express.Application = express();
+
+/** Plugins */
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+/** */
 
 routesHandler(app);
 exceptionHandler(app);
