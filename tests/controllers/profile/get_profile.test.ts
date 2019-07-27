8aeaa8fbeb517a7f3854f390530f365039cb8815
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-07-27 12:40:06 +0700

    update config

--- tests/controllers/profile/get_profile.test.ts
+++ tests/controllers/profile/get_profile.test.ts
@@ -1,15 +1,15 @@
-import test from "ava";
-import * as sinon from "sinon";
-import { HttpError } from "tymon";
+import test from 'ava';
+import * as sinon from 'sinon';
+import { HttpError } from 'tymon';
 
 /** */
-import UserRepository from "../../../src/repositories/user_repo";
-import ProfileController from "../../../src/controllers/profile_controller";
+import UserRepository from '../../../src/repositories/user_repo';
+import ProfileController from '../../../src/controllers/profile_controller';
 
 /** */
 const validContext = {
-    username: "archie",
-    user_id: "123321"
+    username: 'archie',
+    user_id: '123321'
 };
 
 const validData = {
@@ -18,11 +18,11 @@ const validData = {
     body: {}
 };
 
-test.serial("SUCCESS, should retrieve profile data", async (t: any): Promise<void> => {
+test.serial('SUCCESS, should retrieve profile data', async (t: any): Promise<void> => {
     const profile = new ProfileController();
-    const mockUpsert: any = t.context.sandbox.mock(UserRepository.prototype).expects("findOne").resolves({
-        name: "archie",
-        profesion: "software engineer"
+    const mockUpsert: any = t.context.sandbox.mock(UserRepository.prototype).expects('findOne').resolves({
+        name: 'archie',
+        profesion: 'software engineer'
     });
 
     await profile.getProfile(validData, validContext)
@@ -35,13 +35,13 @@ test.serial("SUCCESS, should retrieve profile data", async (t: any): Promise<voi
 
 });
 
-test.serial("FAIL, should throw error if no user data found", async (t: any): Promise<void> => {
+test.serial('FAIL, should throw error if no user data found', async (t: any): Promise<void> => {
     const profile = new ProfileController();
-    const mockUpsert: any = t.context.sandbox.mock(UserRepository.prototype).expects("findOne").resolves(null);
+    const mockUpsert: any = t.context.sandbox.mock(UserRepository.prototype).expects('findOne').resolves(null);
 
     await profile.getProfile(validData, validContext)
         .then((): void => {
-            t.fail("should throw error");
+            t.fail('should throw error');
         })
         .catch((err: any): void => {
             t.true(mockUpsert.called);
@@ -50,14 +50,14 @@ test.serial("FAIL, should throw error if no user data found", async (t: any): Pr
 
 });
 
-test.before("Initialize error handler", (t: any): void => {
+test.before('Initialize error handler', (t: any): void => {
     HttpError.initialize();
 });
 
-test.beforeEach("Initialize New Sandbox Before Each Test", (t: any): void => {
+test.beforeEach('Initialize New Sandbox Before Each Test', (t: any): void => {
     t.context.sandbox = sinon.createSandbox();
 });
 
-test.afterEach.always("Restore Sandbox and Configuration After Each Test", (t: any): void => {
+test.afterEach.always('Restore Sandbox and Configuration After Each Test', (t: any): void => {
     t.context.sandbox.restore();
 });
