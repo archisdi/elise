7d01374f1af90806188b3fab573e7772e333c39d
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-07-27 12:27:48 +0700

    add repo factory

---
+++ src/repositories/index.ts
@@ -0,0 +1,47 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as _ from 'lodash';
+
+interface IObjectString { 
+    [s: string]: string; 
+}
+
+const repoDirPath = '../repositories/';
+let paths: IObjectString;
+
+const normalizeRepoName = (name: string): string => {
+    const splitted = name.split('_').slice(0, -1);
+    return _.camelCase(splitted.join('_'));
+};
+
+const generateRepoPath = (): IObjectString => {
+    if (!paths) {
+        paths = fs.readdirSync(path.join(__dirname, repoDirPath)).reduce((acc: IObjectString, item): object => {
+            const normal: string = normalizeRepoName(item);
+            if (normal) acc[normal] = `${repoDirPath}${item}`;
+            return acc;
+        }, {});
+    }
+    return paths;
+};
+class RepoFactory {
+    private context: any;
+    private instance: { [s: string]: any };
+    private repoPath: IObjectString;
+
+    public constructor(context = {}) {
+        this.context = context;
+        this.instance = {};
+        this.repoPath = generateRepoPath();
+    }
+
+    public get(repoName: string): any {
+        if (!this.instance[repoName]) {
+            const Repo: any = import(this.repoPath[repoName]);
+            this.instance[repoName] = new Repo(this.context);
+        }
+        return this.instance[repoName];
+    }
+}
+
+module.exports = RepoFactory;
