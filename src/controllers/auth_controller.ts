eae4b2ccb2289ea37f8fdbd896453c37d4a25098
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-10-12 19:23:17 +0700

    update auth method

--- src/controllers/auth_controller.ts
+++ src/controllers/auth_controller.ts
@@ -1,10 +1,11 @@
 import { HttpError } from 'tymon';
 
-import JWT from '../libs/jwt';
+import JWT, { validatePassword } from '../libs/jwt';
 import Validator from '../middlewares/request_validator';
-import { IContext, IData, IHandlerOutput } from '../typings/common';
 import BaseController from './base/base_controller';
+import { IContext, IData, IHandlerOutput } from '../typings/common';
 import { LoginHandlerInput } from 'src/typings/methods/auth';
+import UserRepository from '../repositories/user_repo';
 
 export default class AuthController extends BaseController {
     public async login(data: IData, context: IContext): Promise<IHandlerOutput> {
@@ -13,7 +14,18 @@ export default class AuthController extends BaseController {
                 body: { username, password }
             }: LoginHandlerInput = data;
 
-            const token = await JWT.generateToken({ user_id: username });
+            const userRepo = new UserRepository();
+            const user = await userRepo.findOne({ username });
+
+            if (!user) {
+                throw HttpError.NotAuthorized(null, 'CREDENTIAL_NOT_MATCH');
+            }
+
+            if (!validatePassword(password, user.password)) {
+                throw HttpError.NotAuthorized(null, 'CREDENTIAL_NOT_MATCH');
+            }
+
+            const token = JWT.generateToken({ user_id: username });
 
             return {
                 message: 'authentication success',
@@ -23,9 +35,7 @@ export default class AuthController extends BaseController {
                 }
             };
         } catch (err) {
-            if (err.status) {
-                throw err;
-            }
+            if (err.status) throw err;
             throw HttpError.InternalServerError(err.message);
         }
     }
