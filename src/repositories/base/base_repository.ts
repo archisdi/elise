90bedcc027a995baedd764072bcfe55ee000badf
Archie <archie.isdiningrat@gmail.com>
2020-05-14 13:45:22 +0700

    readjust firebase and elastic instance

--- src/repositories/base/base_repository.ts
+++ src/repositories/base/base_repository.ts
@@ -1,14 +1,15 @@
-import { FirebaseContext, ElasticContext } from 'tymon';
 import { DBInstance, getInstance as getDBInstance, getTransaction as getDbTransaction } from 'tymon/modules/db';
 import { MongoInstance, getInstance as getMongoInstance } from 'tymon/modules/mongodb';
 import { RedisInstance, getInstance as getRedisInstance } from 'tymon/modules/redis';
+import { FirebaseInstance, getInstance as getFirebaseInstance } from 'tymon/modules/firebase';
+import { ElasticInstance, getInstance as getElasticInstance } from 'tymon/modules/elastic';
 
 export class BaseRepository {
     private db: DBInstance | null;
     private mongo: MongoInstance | null = null;
     private redis: RedisInstance | null = null;
-    private firebase: any = null;
-    private elastic: any = null;
+    private firebase: FirebaseInstance | null = null;
+    private elastic: ElasticInstance | null = null;
 
     public constructor() {
         this.db = null;
@@ -35,16 +36,16 @@ export class BaseRepository {
         return this.redis;
     }
 
-    public async getFirebaseInstance(): Promise<any> {
+    public async getFirebaseInstance(): Promise<FirebaseInstance> {
         if (!this.firebase) {
-            this.firebase = await FirebaseContext.getInstance();
+            this.firebase = await getFirebaseInstance();
         }
         return this.firebase;
     }
 
-    public async getElasticInstance(): Promise<any> {
+    public async getElasticInstance(): Promise<ElasticInstance> {
         if (!this.elastic) {
-            this.elastic = await ElasticContext.getInstance();
+            this.elastic = await getElasticInstance();
         }
         return this.elastic;
     }
