e4f5ed4afe9f578f95874957dec4c734b40fce96
Archie <archie.isdiningrat@gmail.com>
2020-05-19 21:15:16 +0700

    update test file

--- tests/controllers/profile/get_profile.test.ts
+++ tests/controllers/profile/get_profile.test.ts
@@ -3,7 +3,7 @@ import * as sinon from 'sinon';
 import { HttpError } from 'tymon';
 
 /** */
-import UserRepository from '../../../src/repositories/user_repo';
+import UserRepository from '../../../src/repositories/user_sql_repo';
 import ProfileController from '../../../src/controllers/profile_controller';
 
 /** */
@@ -18,40 +18,46 @@ const validData = {
     body: {}
 };
 
-test.serial('SUCCESS, should retrieve profile data', async (t: any): Promise<void> => {
-    const profile = new ProfileController();
-    const mockUpsert: any = t.context.sandbox.mock(UserRepository.prototype).expects('findOne').resolves({
-        name: 'archie',
-        profesion: 'software engineer'
-    });
-
-    await profile.getProfile(validData, validContext)
-        .then((): void => {
-            t.true(mockUpsert.called);
-        })
-        .catch((err: any): void => {
-            t.fail(err.message);
+test.serial(
+    'SUCCESS, should retrieve profile data',
+    async (t: any): Promise<void> => {
+        const profile = new ProfileController();
+        const mockUpsert: any = t.context.sandbox.mock(UserRepository.prototype).expects('findOne').resolves({
+            name: 'archie',
+            profesion: 'software engineer'
         });
 
-});
+        await profile
+            .getProfile(validData, validContext)
+            .then((): void => {
+                t.true(mockUpsert.called);
+            })
+            .catch((err: any): void => {
+                t.fail(err.message);
+            });
+    }
+);
 
-test.serial('FAIL, should throw error if no user data found', async (t: any): Promise<void> => {
-    const profile = new ProfileController();
-    const mockUpsert: any = t.context.sandbox.mock(UserRepository.prototype).expects('findOne').resolves(null);
+test.serial(
+    'FAIL, should throw error if no user data found',
+    async (t: any): Promise<void> => {
+        const profile = new ProfileController();
+        const mockUpsert: any = t.context.sandbox.mock(UserRepository.prototype).expects('findOne').resolves(null);
 
-    await profile.getProfile(validData, validContext)
-        .then((): void => {
-            t.fail('should throw error');
-        })
-        .catch((err: any): void => {
-            t.true(mockUpsert.called);
-            t.true(err.status === 404);
-        });
-
-});
+        await profile
+            .getProfile(validData, validContext)
+            .then((): void => {
+                t.fail('should throw error');
+            })
+            .catch((err: any): void => {
+                t.true(mockUpsert.called);
+                t.true(err.status === 404);
+            });
+    }
+);
 
 test.before('Initialize error handler', (t: any): void => {
-    HttpError.initialize();
+    // HttpError.initialize();
 });
 
 test.beforeEach('Initialize New Sandbox Before Each Test', (t: any): void => {
