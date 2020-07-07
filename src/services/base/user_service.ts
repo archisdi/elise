7eb8c8b8bac348ebb940b6032eda22d48cd04ec4
Archie <archie.isdiningrat@gmail.com>
2020-07-07 09:15:55 +0700

    implement user service

--- src/services/base/user_service.ts
+++ src/services/base/user_service.ts
@@ -1,7 +1,30 @@
+import RedisRepo from '../../repositories/base/redis_repository';
 import BaseService from './base_service';
 
 export class UserService extends BaseService {
+    private user_id: string;
 
+    public constructor(userId: string) {
+        super();
+        this.user_id = userId;
+    }
+
+    public async setCache<CacheInterface>(cacheName: string, payload: any, expires?: number): Promise<void> {
+        const cacheRepo = new RedisRepo<CacheInterface>(this.user_id);
+        await cacheRepo.setHash(cacheName, payload, expires);
+    }
+
+    public async getCache<CacheInterface = any>(cacheName: string): Promise<CacheInterface | null> {
+        const cacheRepo = new RedisRepo<CacheInterface>(this.user_id);
+        return cacheRepo.getHash(cacheName);
+    }
+
+    public async bustCache<CacheInterface = any>(cacheName?: string): Promise<void> {
+        const cacheRepo = new RedisRepo<CacheInterface>(this.user_id);
+        cacheName ?
+            await cacheRepo.deleteHash(cacheName):
+            await cacheRepo.delete(this.user_id) ;
+    }
 }
 
 export default UserService;
